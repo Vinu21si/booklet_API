164044c8c89648101d80dd5b95e9ed9362ab92a8
Vinu21si <vinayak.fesura954@gmail.com>
2026-02-09 02:17:07 +0530

    dont know

--- src/main/java/com/booklet/booklet/service/booklet_entry_service.java
+++ src/main/java/com/booklet/booklet/service/booklet_entry_service.java
@@ -2,17 +2,18 @@ package com.booklet.booklet.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.lang.NonNull;
 
 import com.booklet.booklet.entity.booklet_entry;
-import com.booklet.booklet.repository.booklet_entry_repository;   // ✅ THIS WAS MISSING
+import com.booklet.booklet.repository.booklet_entry_repository;
 
-@Service   // better than @Component
+@Service
 public class booklet_entry_service {
 
     @Autowired
     private booklet_entry_repository booklet_entry_repository;
 
-    public void saveEntry(booklet_entry booklet_entry) {
+    public void saveEntry(@NonNull booklet_entry booklet_entry) {
         booklet_entry_repository.save(booklet_entry);
     }
 }
